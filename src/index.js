872f32c845a5e0434aed97d8b3cce28d47da81c0
Chris Young <chrisyoung0101@gmail.com>
2020-01-05 11:19:41 -0600

    Video 22 : label text as JS variable inside JSX

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ import ReactDOM from 'react-dom';
 const App = () => {
 
     const buttonText = { text: 'Click me' };
+    const labelText = 'Enter name yo :';
 
     
     return (
         <div> 
-            <label className="label" for="name">Enter name :</label>
+            <label className="label" for="name"> {labelText} </label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor: 'blue', color: 'white'}}>
             {buttonText.text}
